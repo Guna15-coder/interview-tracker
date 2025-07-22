c33f5ec70630dae2149458b848913b0c1530f821
thaneswara <thaneswara31@gmail.com>
2025-07-22 18:58:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn btn-neutral">Neutral</button>
+    </>
+  )
+}
+
+export default App
